d9c9dffaf4bab85559a4df5b800ff25463d4a931
Happy-s0ul <feg200@yandex.ru>
2023-09-19 00:11:45 +0500

    Added localStorage

--- src/js/index.js
+++ src/js/index.js
@@ -29,19 +29,20 @@ function addNewItem() {
     let text = inputForm.value;
     inputForm.value = "";
     if (text !== "") {
-        let item = document.createElement("li");
         if (evenStyleFlag) {
             changeEvenStyle();
         }
         if (oddStyleFlag) {
             changeOddStyle();
         }
+        let item = createNewItem(text);
         item.classList.add("newItem");
-        item.textContent = text;
-        let btn = document.createElement("div");
-        btn.classList.add("selfDeletingButton");
-        item.append(btn);
+        let maxId = (taskList.length > 0) ? Math.max.apply(null, taskList.map(task => task.id)) : -1;
+        maxId++;
+        item.dataset.id = maxId;
         toDoList.prepend(item);
+        taskList.unshift({"id":maxId, "compleated":false});
+        localStorage.setItem(maxId, text);
         if (evenStyleFlag) {
             changeEvenStyle();
         }
@@ -54,10 +55,27 @@ function addNewItem() {
     document.body.style.pointerEvents = "auto";
 }
 
+function createNewItem(textTask) {
+    let item = document.createElement("li");
+    item.textContent = textTask;
+    let btn = document.createElement("div");
+    btn.classList.add("selfDeletingButton");
+    item.append(btn);
+    return item;
+}
+
 function delItem(item, changeStyleFlag = false) {
     return new Promise(function (resolve, reject) {
         document.body.style.pointerEvents = "none";
         item.classList.add("removing");
+        let idItem = item.dataset.id;
+        for (let i=0; i<taskList.length; i++) {
+            if (idItem == taskList[i].id) {
+                localStorage.removeItem(idItem);
+                taskList.splice(i, 1);
+                break
+            }
+        }
         if ((evenStyleFlag || oddStyleFlag) == false) {
             setTimeout(() => {
                 item.remove();
@@ -96,12 +114,16 @@ function changeCompletedStyle(event) {
         delItem(event.target, true).then(
             () => {
                 document.body.style.pointerEvents = "none";
-                if (listItems.length != 0) {
-                    let lastLI = listItems[listItems.length - 1];
+                if (listItems.length != 0) {                
+                    let lastLI = listItems[listItems.length - 1];   
                     lastLI.after(clone);
+                    let idLastLI = taskList.findIndex(item => item.id == lastLI.dataset.id);
+                    taskList.splice(idLastLI+1, 0, {"id":clone.dataset.id, "compleated":false});
                 } else {
                     toDoList.prepend(clone);
+                    taskList.unshift({"id":clone.dataset.id, "compleated":false});
                 }
+                localStorage.setItem(clone.dataset.id, clone.textContent);
                 if (evenStyleFlag) {
                     clone.classList.remove("even");
                     changeEvenStyle();
@@ -118,6 +140,9 @@ function changeCompletedStyle(event) {
         clone.classList.add("completed", "newItem");
         delItem(event.target, true).then(
             () => {
+                document.body.style.pointerEvents = "none";
+                taskList.push({"id":clone.dataset.id, "compleated":true});
+                localStorage.setItem(clone.dataset.id, clone.textContent);
                 toDoList.append(clone);
                 if (evenStyleFlag) {
                     clone.classList.remove("even");
@@ -166,8 +191,48 @@ function changeAddButton() {
     }
 }
 
+function createTaskList() {
+    if (localStorage.length == 0) {
+        let tasks = ["Купить подарок на День рождения","Купить новый стол","Вытащить друзей на прогулку","Научиться готовить торт","Найти новую работу","Прочесть «Дюну»"];
+        for (let task of tasks) {
+            let item = createNewItem(task);
+            item.dataset.id = localStorage.length;
+            toDoList.append(item);
+            taskList[localStorage.length]={"id":localStorage.length,"compleated":false};          
+            localStorage.setItem(localStorage.length, task);
+        }
+        localStorage.setItem("localTaskList", JSON.stringify(taskList));
+        localStorage.setItem("evenStyleFlag", "true");
+        localStorage.setItem("oddStyleFlag", "false");
+        evenStyleFlag = true;
+        evenStyleBox.checked = true;
+        changeEvenStyle();
+    } else {
+        taskList = JSON.parse(localStorage.getItem("localTaskList"));
+        for (let i=0; i<taskList.length; i++) {
+            let item = createNewItem(localStorage.getItem(taskList[i].id));
+            item.dataset.id = taskList[i].id;
+            if (taskList[i].compleated == true) {
+                item.classList.add("completed");
+            }
+            toDoList.append(item);
+        }
+        evenStyleFlag = (localStorage.getItem("evenStyleFlag") === "true") ? true : false;
+        oddStyleFlag = (localStorage.getItem("oddStyleFlag") === "true") ? true : false;
+        if (evenStyleFlag) {
+            evenStyleBox.checked = true;
+            changeEvenStyle();
+        }
+        if (oddStyleFlag) {
+            oddStyleBox.checked = true;
+            changeOddStyle();
+        }
+    }
+}
+
 let evenStyleFlag = false,
     oddStyleFlag = false,
+    taskList = [],
     toDoList = document.getElementById("toDoList"),
     delLastBtn = document.getElementById("delLastBtn"),
     delFirstBtn = document.getElementById("delFirstBtn"),
@@ -185,6 +250,11 @@ oddStyleBox.addEventListener("click", changeOddStyle);
 toDoList.addEventListener("click", delCurrentItem);
 toDoList.addEventListener("click", changeCompletedStyle);
 window.addEventListener("resize", changeAddButton);
+window.addEventListener("visibilitychange", () => {
+    localStorage.setItem("evenStyleFlag", evenStyleFlag);
+    localStorage.setItem("oddStyleFlag", oddStyleFlag);
+    localStorage.setItem("localTaskList", JSON.stringify(taskList));
+});
 
-changeEvenStyle(true);
 changeAddButton();
+createTaskList();
